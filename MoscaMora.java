51d8009a0c7f5b57d8439a7591c6f53af3a79339
Nehikari Guereta <108239522+sqrl13@users.noreply.github.com>
2022-11-16 12:57:04 +0100

    Add files via upload
    El objetivo de esta tarea es realizar un programa que visualice por consola una canción acumulativa y realizar su autoevaluación. Una canción acumulativa es aquella cuyos versos se crean a partir de versos anteriores.
    
    Para ello, tendrás que utilizar métodos y la sentencia println. En concreto usarás métodos para evitar redundancia de manera que solo tengas que escribir cada línea una vez. Por ejemplo, la línea "Y la mora en su moralito sola" aparece varias veces en la canción pero deberías utilizar un único println para mostrarla. No se pueden utilizar métodos que solo contenga una línea de código en su interior.
    También deberás usar métodos para definir la estructura de la canción. Por ejemplo, podrías definir un método por cada estrofa de manera que los únicos println que aparezcan en el main si es que los hay, sean para introducir líneas en blanco.
    
    
    Estaba la mora en su lugar,
    Vino la mosca y le hizo mal,
    La mosca a la mora,
    Y la mora en su moralito sola.
    Estaba la mosca en su lugar,
    Vino la araña y le hizo mal,
    La araña a la mosca,
    La mosca a la mora,
    Y la mora en su moralito sola.
    Estaba la araña en su lugar,
    Vino el ratón y le hizo mal,
    El ratón a la araña,
    La araña a la mosca,
    La mosca a la mora,
    Y la mora en su moralito sola.
    Estaba el ratón en su lugar,
    Vino el gato y le hizo mal,
    El gato al ratón,
    El ratón a la araña,
    La araña a la mosca,
    La mosca a la mora,
    Y la mora en su moralito sola.
    Estaba el gato en su lugar,
    Vino el perro y le hizo mal,
    El perro al gato,
    El gato al ratón,
    El ratón a la araña,
    La araña a la mosca,
    La mosca a la mora,
    Y la mora en su moralito sola.

---
+++ MoscaMora.java
@@ -0,0 +1,59 @@
+/**Nahikari Guereta Calleja*/
+/** Fecha: 26 de septiembre de 2022*/ 
+/** Grado superior de desarrollo de aplicaciones multiplataforma
+Programación, Unidad 1*/ 
+/** Tarea Evaluación 01. Realiza un programa en Java */
+
+package principal;
+public class MoscaMora {   /**Llamamos Mosca Mora a nuestro programa*/
+
+	public static void main(String[] args) {
+	      System.out.println("Estaba la mora en su lugar,");
+	      System.out.println("Vino la mosca y le hizo mal,");
+	      System.out.println("La mosca a la mora,");
+	      System.out.println("Y la mora en su moralito sola.");
+	      System.out.println();
+         
+         
+         
+	      System.out.println("Estaba la mosca en su lugar,");
+	      System.out.println("Vino la araña y le hizo mal,");
+	      System.out.println("La araña a la mosca,");
+	      System.out.println("La mosca a la mora,");
+	      System.out.println("Y la mora en su moralito sola.");
+	      System.out.println();
+         
+         
+         
+	      System.out.println("Estaba la araña en su lugar,");
+	      System.out.println("Vino el ratón y le hizo mal,");
+	      System.out.println("El ratón a la araña,");
+	      System.out.println("La araña a la mosca,");
+	      System.out.println("La mosca a la mora,");
+	      System.out.println("Y la mora en su moralito sola.");
+	      System.out.println();
+         
+         
+         
+	      System.out.println("Estaba el ratón en su lugar,");
+	      System.out.println("Vino el gato y le hizo mal,");
+	      System.out.println("El gato al ratón,");
+	      System.out.println("El ratón a la araña,");
+	      System.out.println("La araña a la mosca,");
+	      System.out.println("La mosca a la mora,");
+	      System.out.println("Y la mora en su moralito sola.");
+	      System.out.println();
+         
+         
+	      System.out.println("Estaba el gato en su lugar,");
+	      System.out.println("Vino el perro y le hizo mal,");
+	      System.out.println("El perro al gato,");
+	      System.out.println("El gato al ratón,");
+	      System.out.println("El ratón a la araña,");
+	      System.out.println("La araña a la mosca,");
+	      System.out.println("La mosca a la mora,");
+	      System.out.println("Y la mora en su moralito sola.");
+	      System.out.println ();
+	}
+	
+}
